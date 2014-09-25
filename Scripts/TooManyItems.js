a6c1633738417e9171cd37e1d4649aafdb47ed1d
Orbitron <admin@team-orbitron.com>
2014-09-25 15:11:49 +0200

    Prepared script to start developement

--- Scripts/TooManyItems.js
+++ Scripts/TooManyItems.js
@@ -1,3 +1,25 @@
 /*
+ * === ABOUT US ===
+ * @name:		Orbitron
+ * @website:	http://www.team-orbitron.com
+ * === ABOUT PROJECT ===
+ * @author:		Orbitron
+ * @project:	TooManyItemsGUI
+ * @version:	v1.0.0
+ * @website:	https://raw.githubusercontent.com/OfficialOrbitron/ModPE/master/Scripts/TooManyItemsGUI.js
+ *
+ * Testet with:
+ * MCPE: v0.9.5
+ * BlockLauncher: v1.7.9
  *
  */
+
+// Project info
+var project		= "TooManyItems";
+var sname		= "TMI";
+var version		= "1.0.0";
+var author		= "Orbitron";
+
+// Values
+
+// Main functions
