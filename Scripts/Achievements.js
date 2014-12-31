e39a50248f1fa1160610d4c74a65c1eaed572300
Orbitron <admin@team-orbitron.com>
2014-12-31 19:14:18 +0100

    .

--- Scripts/Achievements.js
+++ Scripts/Achievements.js
@@ -9,8 +9,8 @@
  * @website:	https://raw.githubusercontent.com/OfficialOrbitron/ModPE/master/Scripts/Achievements.js
  *
  * Testet with:
- * MCPE: v0.9.5
- * BlockLauncher: v1.7.9
+ * MCPE: v0.10.4
+ * BlockLauncher: v1.8.1
  *
  */
 
