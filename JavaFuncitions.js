afe25c865ad6efeb15d2b386d3bca7c665ec3ef4
Orbitron <team-orbitron@hotmail.com>
2015-03-08 11:52:26 +0100

    Update JavaFuncitions.js

--- JavaFuncitions.js
+++ JavaFuncitions.js
@@ -31,46 +31,81 @@ var AppData = {
 // calls "onUseItem(...)"
 function useItem(x, y, z, itemid, blockid, side, itemDamage, blockDamage)
 {
-	if(onUseItem() typeof != 'undefined') onUseItem(x, y, z, itemid, blockid, side, itemDamage, blockDamage);
+	if(typeof onUseItem() != 'undefined') onUseItem(x, y, z, itemid, blockid, side, itemDamage, blockDamage);
 	if(AppData.java && AppData.external) net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod('onUseItem', x, y, z, itemid, blockid, side, itemDamage, blockDamage);
 }
 // calls "onDestroyedBlock(...)"
 function destroyBlock(x, y, z, side)
 {
-	if(onDestroyedBlock() typeof != 'undefined') onDestroyedBlock(x, y, z, side);
+	if(typeof onDestroyedBlock() != 'undefined') onDestroyedBlock(x, y, z, side);
 	if(AppData.java && AppData.external) net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod('onDestroyedBlock', x, y, z, side);
 }
 // calls "onStartDestroyBlock(...)"
 function startDestroyBlock(x, y, z, side)
 {
-	if(onStartDestroyingBlock() typeof != 'undefined') onStartDestroyingBlock(x, y, z, side);
+	if(typeof onStartDestroyBlock() != 'undefined') onStartDestroyingBlock(x, y, z, side);
 	if(AppData.java && AppData.external) net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod('onStartDestroyingBlock', x, y, z, side);
 }
 // calls "onStartNewWorld(...)"
 function newLevel(hasLevel)
 {
-	if(onStartNewWorld() typeof != 'undefined') onStartNewWorld(x, y, z, side);
+	if(typeof onStartNewWorld() != 'undefined') onStartNewWorld(x, y, z, side);
 	if(AppData.java && AppData.external) net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod('onStartNewWorld', hasLevel);
 }
 // calls "onWorldSelected(...)"
 function selectLevelHook()
 {
-	if(onWorldSelected() typeof != 'undefined') onWorldSelected(x, y, z, side);
+	if(typeof onWorldSelected() != 'undefined') onWorldSelected(x, y, z, side);
 	if(AppData.java && AppData.external) net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod('onWorldSelected');
 }
 // calls "onLeavingWorld(...)"
 function leaveGame()
 {
-	if(onLeavingWorld() typeof != 'undefined') onLeavingWorld(x, y, z, side);
+	if(typeof onLeavingWorld() != 'undefined') onLeavingWorld(x, y, z, side);
 	if(AppData.java && AppData.external) net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod('onLeavingWorld');
 }
-function attackHook(attacker, victim) {} //attackCallback *
-function modTick() {} //tickCallback *
-function chatHook(message) {}
-function procCmd(command) {} //chatCallback *
-function deathHook(attacker, victim) {} //mobDieCallback *
-function entityRemovedHook(entity) {} //entityRemovedCallback *
-function entityAddedHook(entity) {} //entityAddedCallback *
+// calls "onAttacking(...)"
+function attackHook(attacker, victim)
+{
+	if(typeof onAttacking() != 'undefined') onAttacking(attacker, victim);
+	if(AppData.java && AppData.external) net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod('onAttacking', attacker, victim);
+}
+// calls "onTick(...)"
+function modTick()
+{
+	if(typeof onTick() != 'undefined') onTick();
+	if(AppData.java && AppData.external) net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod('onTick');
+}
+// calls "onChat(...)"
+¨/*function chatHook(message)
+{
+	if(typeof onChat() != 'undefined') onChat(message);
+	if(AppData.java && AppData.external) net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod('onChat', message);
+}*/
+// calls "onSendCommand(...)"
+function procCmd(command)
+{
+	if(typeof onSendCommand() != 'undefined') onSendCommand(command);
+	if(AppData.java && AppData.external) net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod('onSendCommand', command);
+}
+// calls "onMobDied(...)"
+function deathHook(attacker, victim)
+{
+	if(typeof onMobDied() != 'undefined') onMobDied(attacker, victim);
+	if(AppData.java && AppData.external) net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod('onMobDied', attacker, victim);
+}
+// calls "onEntityRemoved(...)"
+function entityRemovedHook(entity)
+{
+	if(typeof onEntityRemoved() != 'undefined') onEntityRemoved(entity);
+	if(AppData.java && AppData.external) net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod('onEntityRemoved', entity);
+}
+// calls "onEntityAdded(...)"
+function entityAddedHook(entity)
+{
+	if(typeof onEntityAdded() != 'undefined') onEntityAdded(entity);
+	if(AppData.java && AppData.external) net.zhuoweizhang.mcpelauncher.ScriptManager.callScriptMethod('onEntityAdded', entity);
+}
 function levelEventHook(player, eventType, x, y, z, data) {} //levelEventCallback *
 function blockEventHook(x, y, z, type, data) {} //blockEventCallback *
 function serverMessageReceiveHook(message) {} //handleChatPacketCallback *
