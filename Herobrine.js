599d62664f47567d309a6a35a04ae53abad3d641
Orbitron <team-orbitron@hotmail.com>
2015-03-08 11:33:29 +0100

    Update Herobrine.js

--- Herobrine.js
+++ Herobrine.js
@@ -1,25 +1,44 @@
 /*
  * === ABOUT US ===
  * @name:		Orbitron
- * @website:	http://www.team-orbitron.com
+ * @website:	http://www.orbitrondev.com
  * === ABOUT PROJECT ===
- * @author:		Orbitron
+ * @author:		Manuele Vaccari
  * @project:	Herobrine
  * @version:	v1.0.0
- * @website:	https://raw.githubusercontent.com/OfficialOrbitron/ModPE/master/Scripts/Herobrine.js
+ * @website:	https://raw.githubusercontent.com/OrbitronDev/ModPE/master/Herobrine.js
  *
  * Testet with:
- * MCPE: v0.9.5
- * BlockLauncher: v1.7.9
+ * MCPE: v0.10.5
+ * BlockLauncher: v1.8.6
  *
  */
 
 // Project info
-var project		= "Herobrine";
-var sname		= "HB";
-var version		= "1.0.0";
-var author		= "Orbitron";
+var AppInfo = {
+	'projectName': 'Herobrine',
+	'shortName': 'HB',
+	'version': '1.0.0'
+	'author': 'Manuele Vaccari (OrbitronDev)'
+};
 
 // Values
+var AppData = {
+    
+};
+
+// Additional functions
+var AppFunctions = {
+	'printColourMessage': function(sString) {
+		clientMessage(ChatColor.GRAY + '[' + ChatColor.RED + AppInfo.shortName + ChatColor.GRAY + '] ' + ChatColor.WHITE + sString);
+	},
+	'printErrorMessage': function(sString) {
+		clientMessage(ChatColor.GRAY + '[' + ChatColor.RED + AppInfo.shortName + ChatColor.GRAY + '] ' + ChatColor.RED + sString);
+	}
+};
 
 // Main functions
+function newLevel(hasLevel)
+{
+	clientMessage(ChatColor.GRAY + '[INFO] ' + ChatColor.WHITE + AppInfo.projectName + ' loaded');
+}
